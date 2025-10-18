a33a83c407e5483afb4382592cb31b8dde9931e8
Ellipse120 <ellipse120@gmail.com>
2025-10-18 23:01:28 +0800

    fix(首页): fix null.ce error

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,8 @@ export default defineNuxtConfig({
     '@nuxt/content',
     '@vueuse/nuxt',
     'nuxt-og-image',
-    'nuxt-auth-utils'
+    'nuxt-auth-utils',
+    'reka-ui/nuxt'
   ],
 
   devtools: {
